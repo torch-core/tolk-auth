1b0eaff2dc575e65c15c31d2c7eeef35c0b09d3d
ipromise2324 <168allen55@gmail.com>
2025-07-26 01:13:27 +0800

    Add main contract implementation with role authority system
    - Introduced a new contract `main-contract.tolk` that implements a counter example secured by a role authority system.
    - Added internal message handling for counter operations, ownership management, and role capabilities.
    - Updated the entry point in the compiler configuration to reflect the new contract file.

--- wrappers/Main.compile.ts
+++ wrappers/Main.compile.ts
@@ -2,7 +2,7 @@ import { CompilerConfig } from '@ton/blueprint';
 
 export const compile: CompilerConfig = {
     lang: 'tolk',
-    entrypoint: 'contracts/main.tolk',
+    entrypoint: 'contracts/main-contract.tolk',
     withStackComments: true,    // Fift output will contain comments, if you wish to debug its output
     withSrcLineComments: true,  // Fift output will contain .tolk lines as comments
     experimentalOptions: '',    // you can pass experimental compiler options here
